be3f012e568b5b91185cc9c45377cdf400871db0
lukaSavage <tu14181@qq.com>
2024-04-22 23:27:00 +0800

    feat: 处理c语言环境

---
+++ 24 C/chapter01/01_hello.c
@@ -0,0 +1,17 @@
+//
+// Created by lukaSavage on 2024/4/22.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    /**
+     * 这么好用的东西吗，vscode，永远的神！！！
+     *
+     */
+    int a;
+    // printf("你好，世界!%d\n", a);
+    printf("你好%d\n", a);
+    return 0;
+}
