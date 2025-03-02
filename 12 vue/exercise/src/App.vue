daf20864b7801fc71b35ab30c68b500863731fc2
lukaSavage <tu14181@qq.com>
2025-03-02 17:16:01 +0800

    feat: 文档补充

--- 12 vue/exercise/src/App.vue
+++ 12 vue/exercise/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Father from './components/Father/index.vue'
 
 // const count = ref(0)
 // const computedCount = computed(() => {
@@ -30,6 +31,7 @@ console.log(count.value) // 0
   <div>{{ count }}</div>
   <h3>computed:{{ computedCount }}</h3>
   <button @click="handleClick(1, 2, 3, $event)">test</button>
+  <Father></Father>
   <!-- <HelloWorld msg="呵呵呵呵"></HelloWorld> -->
 </template>
 
